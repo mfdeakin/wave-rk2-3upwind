ae0350663cd24182ba58b5d2ba447c63e9ad228b
michael <mfdeakin@cs.unc.edu>
2018-10-29 01:04:43 -0700

    Split the large file up to improve compilation parallelism

---
+++ src/wave_eqn_med.cpp
@@ -0,0 +1,22 @@
+
+#include "wave_eqn_internal.hpp"
+
+// The following 8 mesh sizes were used to determine when the l2 error goes
+// beneath 1e-3
+template class Mesh<512>;
+template class WaveEqnSolver<512, Part1>;
+template class Mesh<640>;
+template class WaveEqnSolver<640, Part1>;
+template class Mesh<704>;
+template class WaveEqnSolver<704, Part1>;
+// Error is less than 1e-3 for 712 cells
+template class Mesh<712>;
+template class WaveEqnSolver<712, Part1>;
+template class Mesh<720>;
+template class WaveEqnSolver<720, Part1>;
+template class Mesh<736>;
+template class WaveEqnSolver<736, Part1>;
+template class Mesh<768>;
+template class WaveEqnSolver<768, Part1>;
+template class Mesh<1024>;
+template class WaveEqnSolver<1024, Part1>;
